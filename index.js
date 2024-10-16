4db44a20488067628c0b5acebb5ec046978c4799
Vashae <vashaeb2@gmail.com>
2024-10-16 15:46:35 -0400

    new java

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-
 const movieListEl = document.querySelector(`movie__picture`)
-function showmovies (movieTitle){
-    window.localStorage.setItem("id", movieTitle);}
-const id = localStorage.getItem("id")
 
 
-async function onSearchChange(event){
-    const id = event.target.value;
-    mainstreet("id")
-
-}
-
-async function mainstreet(id) {
-    
-        const searches = await fetch(`http://img.omdbapi.com/?apikey=baaa7316&s=fast:${id}`);
-        const searchesData = await searches.json();
-        movieListEl.innerHTML = searchesData.map (movie__picture => search(id)).join("")
-    
-
-}
 async function main() {
     try {const movies = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=baaa7316&s=fast");
     const data = await movies.json();
@@ -28,7 +10,7 @@ async function main() {
     const moviecontainer = document.querySelector(`.movie`);
     moviecontainer.innerHTML = data.Search
     .map(
-   (movie) => (`<div class="movie__picture">
+   (movie) => (`<div class="movie__picture onclick = "showmovies${movie.Title}">
         <div class="movie__container">
             <h4> <img src="${movie.Poster}" alt="${movie.Title}"> </h4>
             <h4>${movie.Title}ter</b></h4>
@@ -45,4 +27,19 @@ async function main() {
     document.querySelector('.movie').innerHTML = `<p>Error: ${error.message}</p>`;
 
 }}
-main("id");
+function showmovies (movies){
+    window.localStorage.setItem("id", movies);}
+const id = localStorage.getItem("id")
+
+async function moviePicture(id) {
+    const searches = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=baaa7316&s=fast${id}`);
+    const searchesData = await searches.json();
+}
+
+
+async function onSearchChange(event){
+    const id = event.target.value;
+   moviePicture(id)
+   
+}
+main();
