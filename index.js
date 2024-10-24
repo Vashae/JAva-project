49a6972fcb329f1f0a2ea81092c66dbac4c7d924
Vashae <vashaeb2@gmail.com>
2024-10-24 14:17:28 -0400

    css changes as well as java progress

--- index.js
+++ index.js
@@ -1,50 +1,32 @@
-const movieListEl = document.querySelector(`movie__picture`)
+ async function onSearchChange(event){
+    const id = event.target.value;
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?apikey=baaa7316&s=${id}`);
+        const data = await response.json();
 
+        if (data.Response === "False") {
+            throw new Error(data.Error);
+        }
 
-async function main() {
-    try {const movies = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=baaa7316&s=fast");
-    const data = await movies.json();
-    if (data.response === 'false') {
-        throw new Error(data.Error);
+        const movieContainer = document.querySelector('.movie');
+        movieContainer.innerHTML = data.Search
+            .map((movie) => `
+                <div class="movie__picture">
+                    <div class="movie__container">
+                        <h4></b> <img src="${movie.Poster}" alt="${movie.Title} poster"></h4>
+                        <h4>${movie.Title}</h4>
+                        <p><b>Year:</b> ${movie.Year}</p>
+                        <p><b>imdbID:</b> ${movie.imdbID}</p>
+                        <p><b>Type:</b> ${movie.Type}</p>
+                    </div>
+                </div>
+            `)
+            .join("");
+    } catch (error) {
+        console.error("Error fetching movie data:", error);
+        document.querySelector('.movie').innerHTML = `<p>Error: ${error.message}</p>`;
     }
-    async function moviePicture(id) {
-        const searches = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=baaa7316&s=${id}`);
-        const searchesData = await searches.json();
-    }
-    
-    
-   async function onSearchChange(event){
-        const id = event.target.value;
-       moviePicture(id)
-       console.log(id)
-    }
-    const moviecontainer = document.querySelector(`.movie`);
-    moviecontainer.innerHTML = data.Search
-    .map(
-   (movie) => (`<div class="movie__picture onclick = "showmovies${movie.Title}">
-        <div class="movie__container">
-            <h4> <img src="${movie.Poster}" alt="${movie.Title}"> </h4>
-            <h4>${movie.Title}ter</b></h4>
-            <p><b>Year:</b>${movie.Year}</p>
-            <p><b>imdbID:</b>${movie.imdbID}</p>
-            <p><b>Type:</b>${movie.Type}</p>
-            
-        </div>
-         </div>`))
-
-         .join("");
-   }    catch (error) {
-    console.error("Error fetching movie data:", error);
-    document.querySelector('.movie').innerHTML = `<p>Error: ${error.message}</p>`;
-
-}}
-function showmovies (movies){
-    window.localStorage.setItem("id", movies);}
-const id = localStorage.getItem("id")
-for (let i = 6; i < Math.floor(id); i++){ 
-    id = id +"";
 }
-   
 
 
-main(id);
+
